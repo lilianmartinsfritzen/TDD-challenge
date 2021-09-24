06bd2a15a665c5c78c24ed280bf2f0ed07f06f30
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-24 12:09:18 -0300

    Delete in the service and delete in the created controller. Not Found and Integrity Violation service exceptions created.

--- bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -13,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
