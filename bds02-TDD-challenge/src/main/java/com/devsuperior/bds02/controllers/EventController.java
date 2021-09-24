ef2908fc4ca90905b2cbab5e5e2be009a175ba98
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-24 12:45:28 -0300

    EventController, findAll, findById and update created.

--- bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,5 +1,40 @@
 package com.devsuperior.bds02.controllers;
 
-public class EventController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.dto.EventDTO;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+	
+	@Autowired
+	private EventService service;
+	
+	@GetMapping
+	public ResponseEntity<List<EventDTO>> findAll() {		
+		List<EventDTO> list = service.findAll();			
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> findById(@PathVariable Long id) {
+		EventDTO dto = service.findById(id);		
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 }
