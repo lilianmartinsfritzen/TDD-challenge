0255335f4191615beebc2f5d2585cec9709255d3
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-24 12:02:42 -0300

    Delete in Service and Delete in Controller created.

--- bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,11 @@ public class CityController {
 	public ResponseEntity<List<CityDTO>> findAll() {		
 		List<CityDTO> list = service.findAll();			
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ bds02-TDD-challenge/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.CityDTO;
@@ -23,5 +25,18 @@ public class CityService {
 									// Variação do findAll utilizando o Sort.by para passarmos a ordenação desejada.
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
